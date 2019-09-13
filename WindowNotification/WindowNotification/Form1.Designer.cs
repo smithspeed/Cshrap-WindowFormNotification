71c7eecbf9624a033256dc3cfdc0be6724f6cfbb
dhananjay-at-885838946818 <dhananjay@faithconsultancy.in>
2019-09-13 17:17:08 +0530

    2nd commit

--- WindowNotification/WindowNotification/Form1.Designer.cs
+++ WindowNotification/WindowNotification/Form1.Designer.cs
@@ -29,11 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 261);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- WindowNotification/WindowNotification/Form1.cs
+++ WindowNotification/WindowNotification/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,31 @@ namespace WindowNotification
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected void DisplayNotify()
+        {
+            try
+            {
+                //notifyIcon1.Icon = new System.Drawing.Icon(Path.GetFullPath(@"image\graph.ico"));
+                notifyIcon1.Text = "Testing";
+                notifyIcon1.Visible = true;
+
+                notifyIcon1.BalloonTipTitle = "This is Test by Dhananjay";
+                notifyIcon1.BalloonTipText = "Click here to see message";
+                notifyIcon1.ShowBalloonTip(100);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayNotify();
         }
     }
 }
